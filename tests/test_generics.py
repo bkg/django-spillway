0fa321a4b24ee75b2cd725865f463c29e5f04406
Brian Galey <bkgaley@gmail.com>
2014-07-21 16:03:09 -0700

    Add geojson response test

--- tests/test_generics.py
+++ tests/test_generics.py
@@ -1,8 +1,11 @@
+import json
+
 from django.test import TestCase
 from rest_framework.settings import api_settings
 from rest_framework.test import APIRequestFactory
 
 from spillway import generics
+from spillway.renderers import GeoJSONRenderer
 from .models import Location
 
 factory = APIRequestFactory()
@@ -27,6 +30,15 @@ class GeoListViewTestCase(TestCase):
     def test_paginate(self):
         view = PaginatedGeoListView.as_view(model=Location)
         request = factory.get('/', {'page': 2})
-        response = view(request)
+        response = view(request).render()
         self.assertEqual(len(response.data['results']),
                          PaginatedGeoListView.paginate_by)
+
+    def test_geojson(self):
+        for request in (factory.get('/', {'format': 'geojson'}),
+                        factory.get('/', HTTP_ACCEPT=GeoJSONRenderer.media_type)):
+            response = self.view(request).render()
+            #self.assertEqual(response.data['features'],
+            d = json.loads(response.content)
+            self.assertEqual(d['features'][0]['geometry'],
+                             json.loads(self.qs[0].geom.geojson))
