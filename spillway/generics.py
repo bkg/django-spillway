9fe9ca63041a8e59415bed7c437b31cb8de8fb3d
Brian Galey <bkgaley@gmail.com>
2014-08-12 17:58:35 -0700

    Use a mixin for adding generic model serializer behavior

--- spillway/generics.py
+++ spillway/generics.py
@@ -4,25 +4,10 @@ from rest_framework.settings import api_settings
 from spillway import filters, forms, mixins, renderers, serializers
 
 
-class BaseGeoView(mixins.QueryFormMixin):
+class BaseGeoView(mixins.QueryFormMixin, mixins.GenericSerializerMixin):
     """Base view for models with geometry fields."""
     serializer_class = serializers.FeatureSerializer
     query_form_class = forms.GeometryQueryForm
-
-    def initial(self, request, *args, **kwargs):
-        super(BaseGeoView, self).initial(request, *args, **kwargs)
-        # Attempt to set the model if the serializer has one defined.
-        if not self.model:
-            self.model = getattr(self.serializer_class.Meta, 'model', None)
-
-    def get_serializer_class(self):
-        cls = self.serializer_class
-        # Make sure we have a model set for the serializer, allows calling
-        # .as_view() with a model param only since serializer_class is already
-        # set here.
-        if self.model and getattr(cls.Meta, 'model', False) != self.model:
-            cls.Meta.model = self.model
-        return cls
 
     def wants_default_renderer(self):
         """Returns true when using a default renderer class."""
--- spillway/mixins.py
+++ spillway/mixins.py
@@ -1,13 +1,27 @@
+class GenericSerializerMixin(object):
+    """Allows use of a model serializer class without a predefined model."""
+
+    def get_serializer_class(self):
+        cls = self.serializer_class
+        # Make sure we have a model set for the serializer which allows the use
+        # of additional default serializers than
+        # settings.DEFAULT_MODEL_SERIALIZER_CLASS alone.
+        if self.model and getattr(cls.Meta, 'model', False) != self.model:
+            cls.Meta.model = self.model
+        return cls
+
+
 class QueryFormMixin(object):
     """Mixin to provide form based handling of GET or POST requests."""
     query_form_class = None
 
     def get_query_form(self):
-        """Returns a validated form dict or an empty dict."""
+        """Returns a bound form instance."""
         return self.query_form_class(
             self.request.QUERY_PARAMS or self.request.DATA,
             self.request.FILES or None)
 
     def clean_params(self):
+        """Returns a validated form dict or an empty dict."""
         form = self.get_query_form()
         return form.cleaned_data if form.is_valid() else {}
