33951fa471e805fb305d4c698ebe3a71eb924261
Brian Galey <bkgaley@gmail.com>
2014-08-29 17:40:07 -0700

    Add test for mapnik based tiling

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,14 +1,17 @@
 import json
+from io import BytesIO
 
+from django.core.files import File
 from rest_framework.test import APIRequestFactory, APITestCase
+from PIL import Image
 
 from spillway import views
-from .models import Location
+from .models import Location, RasterStore
+from .test_serializers import RasterTestBase
 
 
 class TileViewTestCase(APITestCase):
     def setUp(self):
-        self.view = views.TileView.as_view(model=Location)
         self.geometry = {'type': 'Polygon',
                          'coordinates': [[ [14.14, 50.21],
                                            [14.39, 49.76],
@@ -17,8 +20,25 @@ class TileViewTestCase(APITestCase):
         Location.create(name='Prague', geom=self.geometry)
 
     def test_response(self):
-        response = self.client.get('/tiles/10/553/347/')
+        response = self.client.get('/vectiles/10/553/347/')
         d = json.loads(response.content)
         # This particular geometry clipped to a tile should have +1 coords.
         self.assertEqual(len(d['features'][0]['geometry']['coordinates'][0]),
                          len(self.geometry['coordinates'][0]) + 1)
+
+
+class MapViewTestCase(RasterTestBase, APITestCase):
+    def setUp(self):
+        super(MapViewTestCase, self).setUp()
+        RasterStore.objects.create(image=File(self.f))
+
+    def test_response(self):
+        response = self.client.get('/maptiles/1/11/342/790/')
+        self.assertEqual(response['content-type'], 'image/png')
+        im = Image.open(BytesIO(response.content))
+        self.assertEqual(im.size, (256, 256))
+
+    def test_no_tile(self):
+        # FIXME: Should this return 404 or 200 with an empty tile?
+        response = self.client.get('/maptiles/10/553/347/')
+        self.assertEqual(response.status_code, 404)
--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,13 @@
 from rest_framework.compat import patterns, url
 from spillway import views
 
-from .models import Location
+from .models import Location, RasterStore
+
+_tile = r'(?P<z>\d+)/(?P<x>\d+)/(?P<y>\d+)/$'
 
 urlpatterns = patterns('',
-    url(r'^tiles/(?P<z>\d+)/(?P<x>\d+)/(?P<y>\d+)/$',
+    url(r'^vectiles/%s' % _tile,
         views.TileView.as_view(model=Location), name='location-tiles'),
+    url(r'^maptiles/(?P<pk>\d+)/%s' % _tile,
+        views.MapView.as_view(model=RasterStore), name='map-tiles'),
 )
