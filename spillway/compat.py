7e19793c16aaccffe09d02978488714ba8914f13
Brian Galey <bkgaley@gmail.com>
2015-01-06 15:45:22 -0800

    Move class def to else block

--- spillway/compat.py
+++ spillway/compat.py
@@ -3,15 +3,14 @@ from rest_framework.utils import encoders
 
 try:
     import simplejson as json
-
+except ImportError:
+    import json
+    JSONEncoder = encoders.JSONEncoder
+else:
     # Workaround to support simplejson 2.2+, see
     # https://github.com/simplejson/simplejson/issues/37
     class JSONEncoder(json.JSONEncoder):
         default = encoders.JSONEncoder().default
-
-except ImportError:
-    import json
-    JSONEncoder = encoders.JSONEncoder
 
 try:
     import mapnik
