9b6a141e579c81f707ebb53c37c1fe9f0a79d6ba
Brian Galey <bkgaley@gmail.com>
2015-04-29 16:58:12 -0700

    Test SpatialLookupFilter with invalid geometry

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from rest_framework import exceptions
 from rest_framework.test import APIRequestFactory
 
 from spillway import generics, filters
@@ -9,14 +10,21 @@ factory = APIRequestFactory()
 
 class FilterTestCase(TestCase):
     def setUp(self):
+        self.filter = filters.SpatialLookupFilter()
         self.view = generics.GeoListView.as_view(model=Location)
         Location.create()
         self.centroid = Location.objects.centroid()[0].centroid
+        self.queryset = Location.objects.all()
 
     def test_spatial_lookup(self):
         request = factory.get('/', {'contains': self.centroid.wkt})
         ctx = self.view(request).renderer_context
-        queryset = Location.objects.all()
-        spfilt = filters.SpatialLookupFilter()
-        qcontain = spfilt.filter_queryset(ctx['request'], queryset, self.view)
-        self.assertQuerysetEqual(qcontain, map(repr, queryset))
+        qcontain = self.filter.filter_queryset(ctx['request'],
+                                               self.queryset, self.view)
+        self.assertQuerysetEqual(qcontain, map(repr, self.queryset))
+
+    def test_invalid_value(self):
+        request = factory.get('/', {'contains': 2})
+        ctx = self.view(request).renderer_context
+        self.assertRaises(exceptions.ParseError, self.filter.filter_queryset,
+                          ctx['request'], self.queryset, self.view)
