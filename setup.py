388c45a3ae2423d96225c35804915ded7942b7a7
Brian Galey <bkgaley@gmail.com>
2015-11-02 17:18:23 -0800

    Release version 0.5.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-spillway',
-      version='0.5.2',
+      version='0.5.3',
       description='Geodata extensions for Django REST Framework',
       long_description=open('README.rst').read(),
       author='Brian Galey',
