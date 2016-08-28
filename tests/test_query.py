4b2a7f5be81cdd34f4458c0d963a86b11c79ac4c
Brian Galey <bkgaley@gmail.com>
2016-08-28 11:46:01 -0700

    Separate summary test methods

--- tests/test_query.py
+++ tests/test_query.py
@@ -88,10 +88,12 @@ class RasterQuerySetTestCase(RasterStoreTestBase):
         qs = self.qs.summarize(self.object.geom.centroid)
         arraycenters = [12, 37, 62]
         self.assertEqual(list(qs[0].image), arraycenters)
+
+    def test_summarize_polygon(self):
         geom = self.object.geom.buffer(-3)
         qs = self.qs.summarize(geom, 'mean')
         means = [9, 34, 59]
-        self.assertEqual(list(qs[0].image), means)
+        self.assertEqual(qs[0].image.tolist(), means)
 
     def test_warp(self):
         qs = self.qs.warp(format='img', srid=3857)
