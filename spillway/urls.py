00e08f5c0baec27f4cc24ed986cbb9ebe7f005b6
Brian Galey <bkgaley@gmail.com>
2016-09-20 18:37:01 -0700

    Add convenience function for building tile urls

---
+++ spillway/urls.py
@@ -0,0 +1,13 @@
+# URL path separator, cannot rely on os.sep as it varies by platform.
+sep = r'/'
+# Regex for an optional file extension.
+format_suffix = r'(?:\.(?P<format>[\w.]+))?'
+# Regex groups for a tile URL in {z}/{x}/{y}.{format} order.
+tile = ('(?P<z>\d+)',
+        '(?P<x>\d+)',
+        '(?P<y>\d+)%s/?$' % format_suffix)
+tileregex = sep.join(tile)
+
+def tilepath(regex):
+    """Appends a tile path regex to a url path."""
+    return r''.join((regex, tileregex))
--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
-from spillway import generics, views
+from spillway import views
+from spillway.urls import tilepath
 
 from .models import Location, RasterStore
 from .test_viewsets import RasterViewSet
@@ -8,15 +9,12 @@ from .test_viewsets import RasterViewSet
 router = DefaultRouter()
 router.register(r'rasters', RasterViewSet)
 
-_format_suffix = r'(?:\.(?P<format>[\w.]+))?'
-_tile = r'(?P<z>\d+)/(?P<x>\d+)/(?P<y>\d+)%s/?$' % _format_suffix
-
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^vectiles/%s' % _tile,
+    url(tilepath('^vectiles/'),
         views.TileView.as_view(queryset=Location.objects.all()),
         name='location-tiles'),
-    url(r'^maptiles/(?P<pk>\d+)/%s' % _tile,
+    url(tilepath('^maptiles/(?P<pk>\d+)/'),
         views.MapView.as_view(queryset=RasterStore.objects.all()),
         name='map-tiles'),
 ]
