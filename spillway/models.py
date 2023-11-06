656329e1703c107b77c667cb54c57617fc445830
Jesaja Everling <jeverling@gmail.com>
2023-11-06 16:41:36 -0300

    Django 4 compatibility

--- spillway/models.py
+++ spillway/models.py
@@ -4,7 +4,7 @@ import tempfile
 
 from django.contrib.gis.db import models
 from django.utils.deconstruct import deconstructible
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 import greenwich
 from greenwich.io import MemFileIO
 import numpy as np
